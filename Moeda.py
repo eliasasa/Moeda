d6f9585f04cc4536c66929c0f709468713fa8778
eliasasa <eliasnetoneto1@hotmail.com>
2024-08-15 07:51:47 -0400

    RandomChoice

---
+++ Moeda.py
@@ -0,0 +1,17 @@
+import random
+moeda=['cara', 'coroa']
+
+X=12
+cont=0
+cont2=0
+
+for i in range(X):
+    y=random.choice(moeda)
+    if y == 'cara':
+        #armazenar cara(roberto)
+        cont+=1
+    elif y == 'coroa':
+        #armazenar coroa(roberto)
+        cont2+=1
+
+#print do resultado(roberto)
