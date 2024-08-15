2e39cc4bd148fed76bcc087f1c2f6b7d9e7022df
H3nriqueSB <henrique49880516@aluno.ms.senac.br>
2024-08-15 08:07:02 -0400

    - Alteração

--- Moeda.py
+++ Moeda.py
@@ -1,18 +1,14 @@
-import random
-cc=['cara', 'coroa']
+def jogamoeda():
+  while True:
+    try:
+      moeda = int(input("Digite o número de vezes que a moeda será jogada: "))
+      if moeda > 0:
+        break
+      else:
+        print("Por favor, digite um número inteiro positivo.")
+    except ValueError:
+      print("Entrada inválida. Por favor, digite um número inteiro.")
+  return moeda
 
-moeda=20
-cont=0
-cont2=0
-
-for i in range(moeda):
-    y=random.choice(cc)
-    if y == 'cara':
-        #armazenar cara(roberto)
-        cont+=1
-    elif y == 'coroa':
-        #armazenar coroa(roberto)
-        cont2+=1
-
-#print do resultado(roberto )
-print(cont, cont2)
+moeda = jogamoeda()
+print(f"A moeda será jogada {moeda} vezes.")
